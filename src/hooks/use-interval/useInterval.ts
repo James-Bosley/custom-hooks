0fb5e0205aeb9b18957bf83a6ce364c80f5df5fa
James Bosley <james_bosley@hotmail.co.uk>
2022-08-31 15:40:15 +0100

    Typing updated.

--- src/hooks/use-interval/useInterval.ts
+++ src/hooks/use-interval/useInterval.ts
@@ -4,24 +4,23 @@ import { useEffect, useState } from "react";
  * This function is a wrapper around the javascript built in setInterval which automatically clears the interval
  * when the component that calls it is unmounted.
  *
- * @param {Function} cb A callback function to call after each interval.
- * @param {Number} delay Interval between function invocations.
+ * @param cb A callback function to call after each interval.
+ * @param delay Interval between function invocations.
  *
- * @return {Number} A reference to the created interval, which can be passed to the javascript clearInterval.
+ * @return {number} A reference to the created interval, which can be passed to the javascript clearInterval.
  */
 
-function useInterval(cb: Function, delay: number): number {
-  const [timer, setTimer] = useState<number>(0);
+function useInterval(cb: () => any, delay: number): NodeJS.Timer {
+  const [timer, setTimer] = useState<NodeJS.Timer | null>(null);
 
   useEffect(() => {
     const timeout = setInterval(cb, delay);
     setTimer(timeout);
 
     return () => clearInterval(timeout);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [cb, delay]);
 
-  return timer;
+  return timer as NodeJS.Timer;
 }
 
 export default useInterval;
--- src/hooks/use-timeout/useTimeout.ts
+++ src/hooks/use-timeout/useTimeout.ts
@@ -4,24 +4,23 @@ import { useEffect, useState } from "react";
  * This function is a wrapper around the javascript built in setTimeout which automatically clears the timeout
  * when the component that calls it is unmounted.
  *
- * @param {Function} cb A callback function to call after the specified delay.
- * @param {Number} delay Minimum duration of delay in ms before callback function is invoked.
+ * @param cb A callback function to call after the specified delay.
+ * @param delay Minimum duration of delay in ms before callback function is invoked.
  *
- * @return {Number} A reference to the created timeout, which can be passed to the javascript clearTimeout.
+ * @return {number} A reference to the created timeout, which can be passed to the javascript clearTimeout.
  */
 
-function useTimeout(cb: Function, delay: number): number {
-  const [timer, setTimer] = useState<number>(0);
+function useTimeout(cb: () => any, delay: number): NodeJS.Timeout {
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const timeout = setTimeout(cb, delay);
     setTimer(timeout);
 
     return () => clearTimeout(timeout);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [cb, delay]);
 
-  return timer;
+  return timer as NodeJS.Timeout;
 }
 
 export default useTimeout;
